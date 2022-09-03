3742c590524bcab59c73a1181f5169f4b3c3e1b4
Shankar Ganesh <shankar.ganesh@seamoney.com>
2022-09-03 15:43:31 +0800

    add secret keys for google oauth2

--- pkg/config/constants.go
+++ pkg/config/constants.go
@@ -1,14 +1,29 @@
 package config
 
 import (
+	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"log"
 )
+
+func viperEnvVariable(key string) string {
+	viper.SetConfigFile("../.env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+	value, ok := viper.Get(key).(string)
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+	return value
+}
 
 func SetupConfig() *oauth2.Config {
 	conf := &oauth2.Config{
-		ClientID:     "test", // to update
-		ClientSecret: "test", // to update
+		ClientID:     viperEnvVariable("client_id"),
+		ClientSecret: viperEnvVariable("client_secret"),
 		RedirectURL:  "https:localhost:9010/google/callback",
 		Scopes: []string{
 			"https://www.googleapis.com/auth/userinfo.email",
